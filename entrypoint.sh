178250af23fe134d874d2e78c5a943ca7c685b46
LoV432 <60856741+LoV432@users.noreply.github.com>
2021-12-05 17:15:07 +0500

    install cod4x18_dedrun if not found

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
-## linuxgsm-docker base image entrypoint script
-## execute LinuxGSM or arbitrary server commands at will
-## by passing command
-
-
-## Because of a limitation in LinuxGSM script it must be run from the directory
-## It is installed in.
-##
-## If one wants to use a volume for the data directory, which is the home directory
-## then we must keep a backup copy of the script on local drive
 echo "-----BEGIN-----"
 if [ ! -e ~/linuxgsm.sh ]; then
     echo "Initializing Linuxgsm User Script in New Volume"
     cp /opt/linuxgsm/linuxgsm.sh ./linuxgsm.sh
-else
+    /home/linuxgsm/linuxgsm.sh cod4server
+fi
+
+if [ -e ~/cod4server* ]; then
+    if [ ! -e ~/serverfiles/cod4x18_dedrun ]; then
+        /home/linuxgsm/cod4server* ai
+        /home/linuxgsm/cod4server* start
+    fi
     /home/linuxgsm/cod4server* ul
-    /home/linuxgsm/cod4server* start
-fi
-
-# with no command, just spawn a running container suitable for exec's
-if [ $# = 0 ]; then
+    /home/linuxgsm/cod4server* r
     tail -F /home/linuxgsm/log/console/cod4server*-console.log # "docker logs -f container-name" will give console realtime output
+
 else
-    # execute the command passed through docker
-    "$@"
-
-    # if this command was a server start cmd
-    # to get around LinuxGSM running everything in
-    # tmux;
-    # we attempt to attach to tmux to track the server
-    # this keeps the container running
-    # when invoked via docker run
-    # but requires -it or at least -t
-    tmux set -g status off && tmux attach 2> /dev/null
+    echo "cod4server exec not found k thx bye"
 fi
-
-exec "$@"
