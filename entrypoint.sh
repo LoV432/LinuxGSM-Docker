75cbb2951f33bfb3e27b825d3705d8d9e9ff373d
LoV432 <60856741+LoV432@users.noreply.github.com>
2021-12-26 17:49:04 +0500

    :D

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ if [ -e ~/cod4server* ]; then
     fi
     /home/linuxgsm/cod4server* ul
     /home/linuxgsm/cod4server* r
+    tail -f /dev/null
     #tail -F /home/linuxgsm/log/console/cod4server*-console.log # "docker logs -f container-name" will give console realtime output
 
 else
